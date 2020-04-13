06177a35664ebfab08ca06aa5fb7e8bb97ed4a54
Buhari Alhassan Ahmed <50429337+makajeez@users.noreply.github.com>
2020-04-13 14:47:03 +0100

    Update estimator.js

--- src/estimator.js
+++ src/estimator.js
@@ -15,18 +15,30 @@
 const covid19ImpactEstimator = (data) => {
   const iCurrentlyInfected = data.reportedCases * 10;
   const sCurrentlyInfected = data.reportedCases * 50;
-  const iInfectionsByRequestedTime = iCurrentlyInfected * (2 ** Math.floor(data.timeToElapse / 3));
-  const sInfectionsByRequestedTime = sCurrentlyInfected * (2 ** Math.floor(data.timeToElapse / 3));
-  const iSevereCasesbyRequestedTime = iInfectionsByRequestedTime * 0.15;
-  const sSevereCasesbyRequestedTime = sInfectionsByRequestedTime * 0.15;
-  const iHospitalBedsByRequestedTime = Math.round((data.totalHospitalBeds * 0.35) - iSevereCasesbyRequestedTime);
-  const sHospitalBedsByRequestedTime = Math.round((data.totalHospitalBeds * 0.35) - sSevereCasesbyRequestedTime);
-  const iCasesForICUByRequestedTime = iInfectionsByRequestedTime * 0.05;
-  const sCasesForICUByRequestedTime = sInfectionsByRequestedTime * 0.05;
-  const iCasesForVentilatorsByRequestedTime = Math.trunc(iInfectionsByRequestedTime * 0.02);
-  const sCasesForVentilatorsByRequestedTime = Math.trunc(sInfectionsByRequestedTime * 0.02);
-  const iDollarsInFlight = parseFloat(iInfectionsByRequestedTime * data.region.avgDailyIncomeInUSD * data.region.avgDailyIncomePopulation * data.timeToElapse).toFixed(2);
-  const sDollarsInFlight = parseFloat(sInfectionsByRequestedTime * data.region.avgDailyIncomePopulation * data.region.avgDailyIncomeInUSD * data.timeToElapse).toFixed(2);
+  const iInfectionsByRequestedTime = iCurrentlyInfected *
+        (2 ** Math.floor(data.timeToElapse / 3));
+  const sInfectionsByRequestedTime = sCurrentlyInfected *
+        (2 ** Math.floor(data.timeToElapse / 3));
+  const iSevereCasesbyRequestedTime = Math.trunc(iInfectionsByRequestedTime * 0.15);
+  const sSevereCasesbyRequestedTime = Math.trunc(sInfectionsByRequestedTime * 0.15);
+  const iHospitalBedsByRequestedTime = Math.trunc(
+    (data.totalHospitalBeds * 0.35) - iSevereCasesbyRequestedTime);
+  const sHospitalBedsByRequestedTime = Math.trunc(
+    (data.totalHospitalBeds * 0.35) - sSevereCasesbyRequestedTime);
+  const iCasesForICUByRequestedTime = Math.trunc(iInfectionsByRequestedTime * 0.05);
+  const sCasesForICUByRequestedTime = Math.trunc(sInfectionsByRequestedTime * 0.05);
+  const iCasesForVentilatorsByRequestedTime = Math.trunc(
+    iInfectionsByRequestedTime * 0.02);
+  const sCasesForVentilatorsByRequestedTime = Math.trunc(
+    sInfectionsByRequestedTime * 0.02);
+  const iDollarsInFlight = (iInfectionsByRequestedTime *
+                            data.region.avgDailyIncomeInUSD *
+                            data.region.avgDailyIncomePopulation * 
+                            data.timeToElapse).toFixed(0);
+  const sDollarsInFlight = (sInfectionsByRequestedTime *
+                            data.region.avgDailyIncomePopulation *
+                            data.region.avgDailyIncomeInUSD *
+                            data.timeToElapse).toFixed(0);
 
   const result = {
     impact: {
